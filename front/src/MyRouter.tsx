10b3edf4c898d56e1a9207fffb7e5b1582b31571
Alexis Bonal <bonal.alexis@gmail.com>
2024-06-04 09:31:03 +0200

    feat(front): ajout page d'accueil

--- front/src/MyRouter.tsx
+++ front/src/MyRouter.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Accueil from "./pages/Accueil";
 
-const router = createBrowserRouter([{ path: "/", element: <p>Hello World</p> }]);
+const router = createBrowserRouter([{ path: "/", element: <Accueil /> }]);
 
 function MyRouter() {
   return <RouterProvider router={router} />;
---
+++ front/src/pages/Accueil.tsx
@@ -0,0 +1,9 @@
+function Accueil() {
+  return (
+    <>
+      <h1 className="text-3xl">Accueil</h1>
+    </>
+  );
+}
+
+export default Accueil;
