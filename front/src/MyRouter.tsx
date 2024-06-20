5bcb4b90e4ffc00f304d0aaa983752f8434086f9
Alexis Bonal <bonal.alexis@gmail.com>
2024-06-20 22:24:15 +0200

    feat(front): ajout page événements

--- front/src/MyRouter.tsx
+++ front/src/MyRouter.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Accueil from "./pages/Accueil";
 import Actualites from "./pages/Actualites";
+import Evenements from "./pages/Evenements";
 
 const router = createBrowserRouter([
   { path: "/", element: <Accueil /> },
   { path: "/actus", element: <Actualites /> },
+  { path: "/evenements", element: <Evenements /> },
 ]);
 
 function MyRouter() {
---
+++ front/src/pages/Evenements.tsx
@@ -0,0 +1,9 @@
+function Evenements() {
+  return (
+    <>
+      <h1 className="text-3xl">Événements</h1>
+    </>
+  );
+}
+
+export default Evenements;
