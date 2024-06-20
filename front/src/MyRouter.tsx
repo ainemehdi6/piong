21d2f44e3acf9d06f6310a91308e8196cbb03f88
Alexis Bonal <bonal.alexis@gmail.com>
2024-06-20 22:20:56 +0200

    feat(front): ajout page actualités

--- front/src/MyRouter.tsx
+++ front/src/MyRouter.tsx
@@ -1,7 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Accueil from "./pages/Accueil";
+import Actualites from "./pages/Actualites";
 
-const router = createBrowserRouter([{ path: "/", element: <Accueil /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <Accueil /> },
+  { path: "/actus", element: <Actualites /> },
+]);
 
 function MyRouter() {
   return <RouterProvider router={router} />;
