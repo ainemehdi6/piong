bd298443b378fb5b03ecb3804f7909624c55ca30
Alexis Bonal <bonal.alexis@gmail.com>
2024-06-20 22:26:51 +0200

    feat(front): ajout page contact

--- front/src/MyRouter.tsx
+++ front/src/MyRouter.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Accueil from "./pages/Accueil";
 import Actualites from "./pages/Actualites";
 import Evenements from "./pages/Evenements";
+import Contact from "./pages/Contact";
 
 const router = createBrowserRouter([
   { path: "/", element: <Accueil /> },
   { path: "/actus", element: <Actualites /> },
   { path: "/evenements", element: <Evenements /> },
+  { path: "/contact", element: <Contact /> },
 ]);
 
 function MyRouter() {
