46512c5e6cc8e4d1895d185b7323c1f14c7e3277
Alexis Bonal <bonal.alexis@gmail.com>
2024-06-21 10:52:08 +0200

    feat(front): ajout de la navbar

--- front/src/layouts/PublicLayout.tsx
+++ front/src/layouts/PublicLayout.tsx
@@ -1,9 +1,15 @@
 import { PropsWithChildren } from "react";
+import Navbar from "../components/Navbar/Navbar";
 
 type PublicLayoutProps = PropsWithChildren;
 
 function PublicLayout({ children }: PublicLayoutProps) {
-  return <>{children}</>;
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
 }
 
 export default PublicLayout;
