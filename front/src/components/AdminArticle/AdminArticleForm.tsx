8a2fa38fa6b016142a1fdb61add617be0093a773
EL AINE El Mehdi <ainemehdi6@gmail.com>
2024-07-12 09:20:50 +0200

    feat: add delete artile

--- front/src/components/AdminArticle/AdminArticleForm.tsx
+++ front/src/components/AdminArticle/AdminArticleForm.tsx
@@ -1,18 +1,58 @@
+import { useState } from "react";
 import MDEditor from "@uiw/react-md-editor";
 import React from "react";
 
 const AdminArticleForm = () => {
-  const [value, setValue] = React.useState("**Hello world!!!**");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState<File | null>(null);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const token = localStorage.getItem("token");
+
+    try {
+      const response = await fetch("http://localhost:5000/api/articles", {
+        method: "POST",
+        body: JSON.stringify({
+          title: "test",
+          description: "test",
+          image: "https://www.macapflag.com/blog/wp-content/uploads/2024/02/tennis-de-table-TITRE.jpg",
+        }),
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.ok) {
+        console.log("Article ajouté avec succès");
+        setTitle("");
+        setDescription("");
+        setImage(null);
+        window.location.reload();
+      } else {
+        console.error("Erreur lors de l'ajout de l'article");
+      }
+    } catch (error) {
+      console.error("Erreur lors de l'ajout de l'article:", error);
+    }
+  };
 
   return (
     <div className="h-full w-full py-8   px-8 rounded-xl bg-xlight-gray">
       <h1 className="text-4xl w-full text-center p-4">Ajouter un article</h1>
       <div className="h-auto w-full">
-        <form className="flex flex-col w-full">
+        <form className="flex flex-col w-full" onSubmit={handleSubmit}>
           <label className="text-lg w-full font-medium">Titre</label>
-          <input className="w-3/6 rounded-lg pl-2" type="text" />
+          <input
+            className="w-3/6 rounded-lg pl-2"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
           <label className="text-lg w-full font-medium">Contenu</label>
-          <MDEditor value={value} onChange={(value) => setValue(value || "")} />
+          <MDEditor value={description} onChange={(value: string | undefined) => setDescription(value || "")} />{" "}
           <label className="text-lg w-full font-medium">Image</label>
           <input
             className="block w-2/3 text-sm text-dark-gray rounded-lg cursor-pointer bg-bg-gray focus:outline-none"
